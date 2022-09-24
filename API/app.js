cce56656bd9ed7f66868fe1448cd02c671ee21e5
aquiles099 <41967583+aquiles099@users.noreply.github.com>
2022-09-23 22:52:42 -0300

    Agregando puertos

--- API/app.js
+++ API/app.js
@@ -7,10 +7,12 @@ const API_1 = __importDefault(require("./app/API"));
 const logger_js_1 = __importDefault(require("./logger.js"));
 const prod = process.argv[0] === '/root/.nvm/versions/node/v14.15.0/bin/node';
 if (prod) {
-    logger_js_1.default.info('Server running on port %d', API_1.default.get('port'));
-    console.log('                                                                  ()_()');
-    console.log(`Server running on http://localhost:${API_1.default.get('port')}             (o.o)`);
-    console.log('                                                                  (|_|)*');
+    API_1.default.listen(API_1.default.get('port'), () => {
+        logger_js_1.default.info('Server running on port %d', API_1.default.get('port'));
+        console.log('                                                                  ()_()');
+        console.log(`Server running on http://localhost:${API_1.default.get('port')}             (o.o)`);
+        console.log('                                                                  (|_|)*');
+    });
 }
 else {
     API_1.default.listen(API_1.default.get('port'), () => {
