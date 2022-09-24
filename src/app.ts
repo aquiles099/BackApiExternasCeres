c88778c59dd0cbb8036e4489312df4b5f19ba469
Gerardo Jimenez <gerardo.jimenez@tekus.co>
2022-09-23 21:12:38 -0400

    Quita SSL

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,10 @@ const prod = process.argv[0] === '/root/.nvm/versions/node/v14.15.0/bin/node';
 
 
 if (prod) {
-	const options = {
-		key: fs.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/privkey.pem', 'utf8'),
-		cert: fs.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/cert.pem', 'utf8'),
-	};
-
-	https.createServer(options, API).listen(API.get('port'), () => {
+	logger.info('Server running on port %d', API.get('port'));
 		console.log('                                                                  ()_()');
-		console.log(`app corriendo en el puerto http://localhost:${API.get('port')}    (o.o)`);
+		console.log(`Server running on http://localhost:${API.get('port')}             (o.o)`);
 		console.log('                                                                  (|_|)*');
-	});
-
 } else {
 	API.listen(API.get('port'), () => {
 		logger.info('Server running on port %d', API.get('port'));
