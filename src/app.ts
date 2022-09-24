ca39b78725dac72bd591d730137a09f7663546bf
Gerardo Jimenez <gerardo.jimenez@tekus.co>
2022-09-23 21:54:31 -0400

    [FIX] app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ const prod = process.argv[0] === '/root/.nvm/versions/node/v14.15.0/bin/node';
 
 
 if (prod) {
-	logger.info('Server running on port %d', API.get('port'));
+	API.listen(API.get('port'), () => {
+		logger.info('Server running on port %d', API.get('port'));
 		console.log('                                                                  ()_()');
 		console.log(`Server running on http://localhost:${API.get('port')}             (o.o)`);
 		console.log('                                                                  (|_|)*');
+	});
 } else {
 	API.listen(API.get('port'), () => {
 		logger.info('Server running on port %d', API.get('port'));
