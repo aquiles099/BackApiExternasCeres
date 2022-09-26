86aad3f717b96808bf11d3e2e816c06e22f13c72
Gerardo Jimenez <gerardo.jimenez@tekus.co>
2022-09-26 10:42:45 -0400

    [FIX] Change conect to SSL API Prod

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,21 @@ const prod = process.argv[0] === '/root/.nvm/versions/node/v14.15.0/bin/node';
 
 
 if (prod) {
+	
 	API.listen(API.get('port'), () => {
-		logger.info('Server running on port %d', API.get('port'));
-		console.log('                                                                  ()_()');
-		console.log(`Server running on http://localhost:${API.get('port')}             (o.o)`);
-		console.log('                                                                  (|_|)*');
+
+		const options = {
+			key: fs.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/fullchain.pem', 'utf8'),
+			cert: fs.readFileSync(' /etc/letsencrypt/live/externo.devceres.cloud/privkey.pem', 'utf8'),
+		};
+   
+		https.createServer(options, API).listen(API.get('port'), () => {
+			logger.info('Server running on port %d', API.get('port'));
+			console.log('                                                                  ()_()');
+			console.log(`app corriendo en el puerto https://externo.devceres.cloud:${API.get('port')}    (o.o)`);
+			console.log('                                                                  (|_|)*');
+		});
+
 	});
 } else {
 	API.listen(API.get('port'), () => {
