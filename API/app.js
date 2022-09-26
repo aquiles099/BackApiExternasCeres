f1854f507357f0bff723d4f146768ef0e677df10
Gerardo Jimenez <gerardo.jimenez@tekus.co>
2022-09-26 11:10:21 -0400

    [FIX] Adjust SSL

--- API/app.js
+++ API/app.js
@@ -9,17 +9,15 @@ const API_1 = __importDefault(require("./app/API"));
 const logger_js_1 = __importDefault(require("./logger.js"));
 const prod = process.argv[0] === '/root/.nvm/versions/node/v14.15.0/bin/node';
 if (prod) {
-    API_1.default.listen(API_1.default.get('port'), () => {
-        const options = {
-            cert: fs_1.default.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/fullchain.pem', 'utf8'),
-            key: fs_1.default.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/privkey.pem', 'utf8'),
-        };
-        https_1.default.createServer(options, API_1.default).listen(API_1.default.get('port'), () => {
-            logger_js_1.default.info('Server running on port %d', API_1.default.get('port'));
-            console.log('                                                                  ()_()');
-            console.log(`app corriendo en el puerto https://externo.devceres.cloud:${API_1.default.get('port')}    (o.o)`);
-            console.log('                                                                  (|_|)*');
-        });
+    const options = {
+        key: fs_1.default.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/privkey.pem', 'utf8'),
+        cert: fs_1.default.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/fullchain.pem', 'utf8')
+    };
+    https_1.default.createServer(options, API_1.default).listen(API_1.default.get('port'), () => {
+        logger_js_1.default.info('Server running on port %d', API_1.default.get('port'));
+        console.log('                                                                  ()_()');
+        console.log(`app corriendo en el puerto https://externo.devceres.cloud:${API_1.default.get('port')}    (o.o)`);
+        console.log('                                                                  (|_|)*');
     });
 }
 else {
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import http from 'http';
 import https from 'https';
 import fs from 'fs';
 import API from './app/API';
@@ -8,21 +9,18 @@ const prod = process.argv[0] === '/root/.nvm/versions/node/v14.15.0/bin/node';
 
 if (prod) {
 	
-	API.listen(API.get('port'), () => {
+	const options = {
+		key: fs.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/privkey.pem', 'utf8'),
+		cert: fs.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/fullchain.pem', 'utf8')
+	};
 
-		const options = {
-			cert: fs.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/fullchain.pem', 'utf8'),
-			key: fs.readFileSync('/etc/letsencrypt/live/externo.devceres.cloud/privkey.pem', 'utf8'),
-		};
-   
-		https.createServer(options, API).listen(API.get('port'), () => {
-			logger.info('Server running on port %d', API.get('port'));
-			console.log('                                                                  ()_()');
-			console.log(`app corriendo en el puerto https://externo.devceres.cloud:${API.get('port')}    (o.o)`);
-			console.log('                                                                  (|_|)*');
-		});
-
+	https.createServer(options, API).listen(API.get('port'), () => {
+		logger.info('Server running on port %d', API.get('port'));
+		console.log('                                                                  ()_()');
+		console.log(`app corriendo en el puerto https://externo.devceres.cloud:${API.get('port')}    (o.o)`);
+		console.log('                                                                  (|_|)*');
 	});
+
 } else {
 	API.listen(API.get('port'), () => {
 		logger.info('Server running on port %d', API.get('port'));
